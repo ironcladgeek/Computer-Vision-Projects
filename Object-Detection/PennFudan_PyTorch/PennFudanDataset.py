f0b0d31e911622b17858bc5865d328bf1cef0ef1
M. Reza Zerehpoosh <ironcladgeek@hotmail.com>
2020-02-15 00:49:00 +0330

    Extend Dataset class for PennFudan data

---
+++ Object-Detection/PennFudan_PyTorch/PennFudanDataset.py
@@ -0,0 +1,65 @@
+import os
+import numpy as np
+import torch
+from PIL import Image
+
+class PennFudanDataset(torch.utils.data.Dataset):
+    def __init__(self, root, transforms=None):
+        self.root = root
+        self.transforms = transforms
+        self.images_dir = os.path.join(root, 'PNGImages')
+        self.masks_dir = os.path.join(root, 'PedMasks')
+        self.imgs = list(sorted(os.listdir(self.images_dir)))
+        self.masks = list(sorted(os.listdir(self.masks_dir)))
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.images_dir, self.imgs[idx])
+        mask_path = os.path.join(self.masks_dir, self.masks[idx])
+        img = Image.open(img_path).convert('RGB')
+        mask = np.array(Image.open(mask_path))
+        # instances are encoded as different colors
+        # the first id is background, so remove it
+        obj_ids = np.unique(mask)[1:]
+
+        # split the color-encoded mask into a set of binary masks
+        masks = mask == obj_ids[:, None, None]
+
+        # get bounding box coordinates for each mask
+        num_objs = len(obj_ids)
+        boxes = []
+        for i in range(num_objs):
+            pos = np.where(masks[i])
+            xmin =  np.min(pos[1])
+            xmax = np.max(pos[1])
+            ymin = np.min(pos[0])
+            ymax = np.max(pos[0])
+            boxes.append([xmin, ymin, xmax, ymax])
+
+        # convert everything into a torch.Tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.flot32)
+        # there is only one class (pedestrian)
+        labels = torch.ones((num_objs), dtype=torch.int64)
+        masks = torch.as_tensor(masks, dtype=uint8)
+
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((num_objs), dtype=torch.int64)
+
+        target = {
+            'boxes': boxes,
+            'labels': labels,
+            'masks': masks,
+            'image_id': image_id,
+            'area': area,
+            'iscrowd': iscrowd
+        }
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
+         
